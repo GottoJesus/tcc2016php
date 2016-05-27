023ded18605bea58338cc2540db928f248dce98c
GottoJesus <gdj.jesus5@gmail.com>
2016-05-27 20:25:26 -0300

    Bls

--- tcc2016_php/ferramentas/ConexaoBD.php
+++ tcc2016_php/ferramentas/ConexaoBD.php
@@ -1,1 +1,24 @@
-<?php $conexao = new PDO("mysql:host=localhost;dbname=db_tcc2016", "root", "");?>
+<?php 
+$conexao = new PDO("mysql:host=localhost;dbname=db_tcc2016", "root", "");
+
+function previneSQLInjection($string){
+	
+	$string = str_ireplace("SELECT", "", $string);
+	$string = str_ireplace("INSERT", "", $string);
+	$string = str_ireplace("UPDATE", "", $string);
+	$string = str_ireplace("DELETE", "", $string);
+	$string = str_ireplace(" AND ", "", $string);
+	$string = str_ireplace(" OR ", "", $string);
+	$string = str_ireplace("CREATE", "", $string);
+	$string = str_ireplace("DROP", "", $string);
+	$string = str_ireplace("TABLE", "", $string);
+	$string = str_ireplace(" INTO ", "", $string);
+	$string = str_ireplace("WHERE", "", $string);
+	$string = addslashes($string);
+	$string = strip_tags($string);
+	
+	return $string;
+}
+
+
+?>
